041bcdb64413c0ee75dbfb9e09219064b0afce2c
william.chiang <william.chiang@tcghl.com>
2020-10-13 11:45:55 +0800

    feat(training-enum): add exercise enum

---
+++ src/app/utilities/enums/training.enum.ts
@@ -0,0 +1,59 @@
+export enum ChestExercise {
+    DumbbellBenchPress = 'Dumbbell bench press',
+    BarbellBenchPress = 'barbell bench press',
+    InclineDumbbellBenchPress = 'Incline Dumbbell bench press',
+    DeclineDumbbellBenchPress = 'decline Dumbbell bench press',
+    InclineBarbellBenchPress = 'Incline barbell bench press',
+    DeclineBarbellBenchPress = 'decline barbell bench press',
+    CableFly = 'Cable fly'
+}
+
+export enum ShoulderExercise {
+    DumbbellShoulderPress = 'Dumbbell shoulder press',
+    BarbellShoulderPress = 'barbell shoulder press',
+    SideDumbbellLateralRaise = 'Side Dumbbell lateral raise',
+    FrontDeltDumbbellFly = 'Front delt Dumbbell fly',
+    RearDeltDumbbellFly = 'Rear delt Dumbbell fly',
+}
+
+export enum BackExercise {
+    LatPulldown = 'Lat pulldown',
+    SeatedRowMachine = 'Seated row machine',
+    PullUps = 'Pull ups',
+    Shrugs = 'Shrugs',
+}
+
+export enum LegExercise {
+    Squads = 'Squads',
+    LegPress = 'Leg press',
+    LegExtension = 'Leg extension',
+    LegCurl = 'Leg curl',
+    DeadLifts = 'Dead lifts'
+}
+
+export enum BicepsExercise {
+    BarbellCurls = 'Barbell curls',
+    HammerCurls = 'Hammer curls',
+    DumbbellCurls = 'dumbbell curls',
+    PreacherCurls = 'Preacher curls',
+    ConcentrationCurls = 'Concentration curls'
+}
+
+export enum TricepExercise {
+    TricepCablePushdown = 'Tricep cable pushdown',
+    BenchDips = 'Bench dips',
+    TricepKickbacks = 'Tricep kickbacks'
+}
+
+export enum Cardio {
+    Swiming = 'swiming',
+    Biking = 'biking',
+    Sliding = 'sliding machine',
+    Step = 'step machine'
+}
+
+export enum EW {
+    ChestPress = 'Chest press machine',
+    ShoulderPressMachine = 'shoulder press machine',
+    LegPress = 'Leg press'
+}
